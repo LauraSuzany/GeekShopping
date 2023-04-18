d8d9b0de322b8af8722b7e64a4f809c498dccc83
Laura Suzany <83149455+LauraSuzany@users.noreply.github.com>
2023-04-17 22:07:21 -0300

    Feat: implementando interface de IProductRepository

--- GeekShopping.ProductAPI/Repository/IProductRepository.cs
+++ GeekShopping.ProductAPI/Repository/IProductRepository.cs
@@ -1,6 +1,14 @@
-﻿namespace GeekShopping.ProductAPI.Repository
+﻿using GeekShopping.ProductAPI.Data.ValueObjects;
+
+namespace GeekShopping.ProductAPI.Repository
 {
     public interface IProductRepository
     {
+        Task<IEnumerable<ProductVO>> FundAll();
+        Task<ProductVO> FundById(long id);
+        Task<ProductVO> Create(ProductVO product);
+        Task<ProductVO> Update(ProductVO product);
+        Task<bool> Delete(long id);
+
     }
 }
